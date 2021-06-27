e389377939e8f3094a043e77ce3624812959fb6f
Yamen <yh@razrlab.com>
2021-06-27 20:47:30 +0400

    Added skeleton for Sudoku class

---
+++ src/Sudoku.js
@@ -0,0 +1,16 @@
+"use strict";
+
+class Sudoku {
+  /**
+   * Preliminary API:
+   *
+   * - createSudoku(difficulty)
+   * - newGame()
+   * - restart()
+   * - check (number<1to9>, x, y) -> Boolean
+   *
+   *
+   */
+}
+
+module.exports = Sudoku;
